58e0a7cc3ca0456ec815f9410faa172875b7ba05
fedecostan <38157990Fede>
2019-04-01 11:26:54 -0300

    FACTURA

---
+++ src/main/java/com/sistemas/facturacion/service/afipWS/https/wsaahomo_afip_gov_ar/ws/services/logincms/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "https://wsaahomo.afip.gov.ar/ws/services/LoginCms")
+package com.sistemas.facturacion.service.afipWS.https.wsaahomo_afip_gov_ar.ws.services.logincms;
