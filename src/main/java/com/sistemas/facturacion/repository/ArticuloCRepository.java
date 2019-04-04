eafdd4c8fe858bb5461ed33a26f1a48c4ad08321
Federico Costantino <federico.c@opendevsolutions.com.ar>
2019-04-04 19:05:24 -0300

    Grabar Factura

--- src/main/java/com/sistemas/facturacion/repository/ArticuloCRepository.java
+++ src/main/java/com/sistemas/facturacion/repository/ArticuloCRepository.java
@@ -10,8 +10,17 @@ import java.util.List;
 @Repository
 public interface ArticuloCRepository extends JpaRepository<ArticuloC, Long> {
 
+    @Query(value = "SELECT * FROM articuloc a" +
+            " JOIN articulop p ON a.rubroarticulo = p.rubroarticulo" +
+            " WHERE p.fechadesde <= ?1" +
+            " AND p.fechahasta >= ?1", nativeQuery = true)
     List<ArticuloC> findByFecha(String fecha);
 
+    @Query(value = "SELECT * FROM articuloc a" +
+            " JOIN articulop p ON a.rubroarticulo = p.rubroarticulo" +
+            " WHERE p.fechadesde <= ?2" +
+            " AND p.fechahasta >= ?2" +
+            " AND a.rubroarticulo =?1", nativeQuery = true)
     ArticuloC findByRubroArticuloAndFecha(String formattedNumber, String fecha);
 
     ArticuloC findByRubroArticulo(String codigo);
